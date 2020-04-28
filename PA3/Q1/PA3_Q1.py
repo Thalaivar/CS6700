9c0f57ff0bca77cb56ec75526f9d04d2e49c9df3
Thalaivar <dhruvlaad@gmail.com>
2020-04-28 15:36:00 +0530

    Q1 done

--- PA3/Q1/PA3_Q1.py
+++ PA3/Q1/PA3_Q1.py
@@ -2,9 +2,10 @@ import gym
 import numpy as np
 from options import HallwayOption
 import matplotlib.pyplot as plt
+from matplotlib import colors
 
-N_EPISODES = 10000
-N_RUNS = 30
+N_EPISODES = 1000
+N_RUNS = 100
 
 def main(goal_state):
     env = gym.make("room_world:room-v0")
@@ -14,12 +15,17 @@ def main(goal_state):
     elif goal_state == 2:
         env.goal = env.G2
 
-    steps, Q_table = train(env)
+    steps, Q_table, _ = train(env)
     plt.plot(np.arange(N_EPISODES), steps)
     plt.show()
 
+    np.save("results_Qtable_G2", Q_table)
+    np.save("results_steps_G2", steps)
+
+    visualize(Q_table, env)
+
 def train(env):
-    alpha = 1/32
+    alpha = 1/8
 
     # total of 12 options: 4 primitive actions and 8 multi-step options
 
@@ -28,6 +34,8 @@ def train(env):
     
     # track of steps per episode
     steps = np.zeros((N_EPISODES,))
+    # average Q_table
+    avg_Q_table = np.zeros((13, 13, 12))
 
     for j in range(N_RUNS):
         Q_table = np.zeros((13, 13, 12))
@@ -52,10 +60,11 @@ def train(env):
                 # print(current_steps)
 
             steps[i] += current_steps
-            # if i % 1000 == 0:
-            #     print("Episode: %d"%(i))
+        
+        avg_Q_table +=  Q_table
+            
         print("Run = ", j)
-    return steps/N_RUNS, Q_table, multi_step_options
+    return steps/N_RUNS, avg_Q_table/N_RUNS, multi_step_options
 
 # composite step function to include multi-step options
 def step(env, option, state, multi_step_options):
@@ -124,19 +133,69 @@ def create_options(env):
     
     return options
 
-def visualize(Q_table, env, options):
+def visualize(Q_table, env, options=None):
+    if options is None:
+        options = create_options(env)
+
     grid_size = [Q_table.shape[0], Q_table.shape[1]]
     # plot where options are taken
-    opts_vis_Q_table = np.zeros((grid_size[0], grid_size[1]))
-    
+    vis_Q_table = np.zeros((grid_size[0], grid_size[1]))
+     
+    # primitive_track = []
     for i in range(grid_size[0]):
         for j in range(grid_size[1]):
             state = [i,j]
-            # valid options in this state
+            # mark walls in room world
+            if state in env.walls:
+                vis_Q_table[i,j] = -1
+            else:
+                # valid options in this state
+                valid_opts = valid_options(state, options)
+                # greedily choose best option
+                option = np.argmax(Q_table[i,j,valid_opts])
+                # differentiate between primitive actions and options
+                if option > 3:
+                    option = 1
+                else:
+                    # primitive_track.append([i,j,option])
+                    option = 0
+                vis_Q_table[i,j] = option
+
+    vis_Q_table = vis_Q_table.T
+
+    # colour code the results:
+    # -1  -> walls
+    # 0   -> primitive actions
+    # 1   -> multi - step options
+    cmap = colors.ListedColormap([[0.64,0.45,0.16], [0.17,0.35,0.62], [0.07,0.59,0.47]])
+    plt.figure(figsize=(6,6))
+    plt.pcolor(vis_Q_table, cmap=cmap, edgecolors='k', linewidths=1)
+    ax = plt.gca()
+    ax.axes.xaxis.set_visible(False)
+    ax.axes.yaxis.set_visible(False)
+
+    # legend stuff
+    rect = lambda color: plt.Rectangle((0,0), 1, 1, color=color)
+    plt.legend([rect([0.17,0.35,0.62]), rect([0.07, 0.59, 0.47])], ["primitive actions", "multi-step options"], loc='upper right')
+    plt.show()
+
+    # plot heatmap of max values
+    val_vis_grid = np.zeros(vis_Q_table.shape)
+    for i in range(grid_size[0]):
+        for j in range(grid_size[1]):
             valid_opts = valid_options(state, options)
-            # get best option
+            val_vis_grid[i,j] = np.max(Q_table[i,j,valid_opts])
 
+    plt.pcolormesh(val_vis_grid.T, cmap='viridis', linewidths=1, edgecolors='k')
+    plt.colorbar()
+    ax = plt.gca()
+    labels = [item.get_text() for item in ax.get_xticklabels()]
+    empty_string_labels = ['']*len(labels)
+    ax.set_xticklabels(empty_string_labels)
+    ax.set_yticklabels(empty_string_labels)
+    plt.show()
 
+    # print(primitive_track)
 
 if __name__ == '__main__':
-    main(1)
+    main(2)
